3c9fcee7be7417f41ef36dd4c2c8f13773a98bd4
Xiangyun-Chen <xiangyun_Chen@outlook.com>
2026-05-19 10:30:01 +0800

    add 01_variable_and_type

---
+++ 01_variable_and_type.py
@@ -0,0 +1,16 @@
+# 01_variable_and_type.py
+# 演示 Python 变量与数据类型
+
+# 1. 字面量输出
+print("--- 1. 字面量 ---")
+print(666)
+print(13.14)
+
+# 2. 变量操作
+money = 50
+print(f"钱包还有: {money}") # 建议养成使用 f-string 的习惯
+
+# 3. 类型查看与转换
+name = "黑马程序员"
+print(type(name))
+num_str = str(11) # 转换演示
